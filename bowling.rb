639081e4f38aebbbf9ff2527608ca22c405ffc42
Kemlyn Reid <kemlyn@reel.tech>
2018-11-12 16:21:52 +0000

    scoring added

--- bowling.rb
+++ bowling.rb
@@ -3,4 +3,5 @@ game = Game.new
 
 10.times do
   game.run
+  puts "Total Score: #{game.score.userscore} out of a possible 100"
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,37 +1,34 @@
 # Initializes and starts the game
 class Game
-  def initialize(frame = 10, pin = 10)
-    @frame = frame
-    @pins = pin
-    @score = Game::Score.new
+  attr_accessor :score, :user
+  def initialize
+    @score = Game::Scoring.new
     @user = Game::User.new
   end
 
   def run
-    @score.roll(@user.roll1, @user.roll2)
+    @score.run(@user.roll1, @user.roll2)
   end
 
   # outcomes and scoring
-  class Score
+  class Scoring
     attr_accessor :userscore
     # keeping score
     def initialize
       @userscore = 0
-      @roll1 = []
-      @roll2 = []
     end
 
-    def roll(roll1, roll2)
-      @roll1 << roll1.to_s.split('').map(&:to_i)
-      @roll2 << roll2.to_s.split('').map(&:to_i)
-      if roll1 == 10
-        puts %(STRIKE!)
+    def run(roll1, roll2)
+      if roll1 > 10 || roll2 > 10 || roll1 + roll2 > 10
+        puts 'Invalid input'
+      elsif roll1 == 10
+        puts 'STRIKE!'
         @userscore += 10
       elsif roll1 + roll2 == 10
-        puts %(SPARE!)
+        puts 'SPARE!'
         @userscore += 10
       else
-        puts 'You\'re score is' + " #{roll1 + roll2} " + 'out of a possible 10'
+        puts "You're score is #{roll1 + roll2} out of 10"
         @userscore += (roll1 + roll2)
       end
     end
